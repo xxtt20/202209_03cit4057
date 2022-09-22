372da83469123e69e4c59861a7026c795467e6c1
liyuu-hkct <113425488+liyuu-hkct@users.noreply.github.com>
2022-09-22 15:04:41 +0800

    classwork 1.2-1.4 (#24)
    * Classwork-1.2.
    
    * Classwork-1.3.
    
    * Classwork-1.4.
    
    * Classwork-1.2.
    
    * Classwork-1.6
    
    * notes

---
+++ cw01/src/22557138/simpleinterest.py
@@ -0,0 +1,17 @@
+#1.Try Arithmetic operator
+def simple_interest(p,t,r ) :
+    si = ( p * t * r) /100
+    return si
+
+def main() :
+    principal = 8
+    time = 6
+    rate = 8
+    print ( 'The principal is ' , principal)
+    print ( 'The time period is ' , time)
+    print ( 'The rate of interest is ' , rate)
+    #Try line wrap within parentheses
+    si = simple_interest( principal, time,rate)
+    print ( 'The simple Interest is' , si)
+if __name__=="__main__":
+    main()
