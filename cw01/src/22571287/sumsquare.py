42327ba0bcc76ccc396268b2208138489c246f85
QiuXiaoJun80 <113962121+QiuXiaoJun80@users.noreply.github.com>
2022-09-22 15:01:00 +0800

    Update students.csv (#16)
    * task01 and classwork 01

---
+++ cw01/src/22571287/sumsquare.py
@@ -0,0 +1,21 @@
+#1. Try comment
+'''
+This program calculates the sum of 
+square of first N natural 
+number
+'''
+
+def squaresum(n) : 
+    sum = 0 
+    for i in range(1, n+1) : 
+        sum = sum + (i * i )
+    return sum 
+
+def main():
+    integerN = 4 
+    # Try line wrap within parentheses
+    print("The sum of square of integer N is ",
+        squaresum(integerN))
+        
+if __name__ == "__main__":
+    main()
