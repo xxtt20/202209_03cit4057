4121f50367dc7d06a39a37405d85948c4d340619
kit7689809 <113425502+kit7689809@users.noreply.github.com>
2022-09-22 15:08:28 +0800

    classwwork-1.3. (#28)

---
+++ cw01/src/22571724/findmax.py
@@ -0,0 +1,13 @@
+def maximum(num1,num2):
+    if num1 >= num2 :
+        return num1
+    else:
+        return num2
+
+def main():
+    number1 =1000
+    number2=2919
+    print(maximum(number1, number2))
+
+if __name__ == "__main__" :
+    main()
