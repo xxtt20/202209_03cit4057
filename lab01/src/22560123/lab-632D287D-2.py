650a2cb26e43ea3f2f6b622c94dd438bae07eda6
unknown <2957839161@qq.com>
2022-09-27 17:39:01 +0800

    lab01

---
+++ lab01/src/22560123/lab-632D287D-2.py
@@ -0,0 +1,14 @@
+def getString() :
+    ret = ""
+    # START: Write your code here
+    str1 = "Holly"
+    str2 = "Dolly"
+    ret =str1+str2
+    # END: Write you code here
+    return ret
+
+def main():
+    print( getString())
+
+if __name__ == "__main__":
+    main()
