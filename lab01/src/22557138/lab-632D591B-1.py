136714463a5414386e0f19fb5614d3f9985be700
liyuu-hkct <113425488+liyuu-hkct@users.noreply.github.com>
2022-09-28 17:24:21 +0800

    Lab Worksheet 01 (#35)
    * Classwork-1.2.
    
    * Classwork-1.3.
    
    * Classwork-1.4.
    
    * Classwork-1.2.
    
    * Classwork-1.6
    
    * notes
    
    * Lab-632D591B: Comparisons and Booleans
    
    * Lab-632D376E: String Concatenations
    
    * Lab-632D7372 : String Join() and Split() Methods
    
    * Lab-632D64FF: Indentation
    
    * Lab-632D287D: Getting Started with Variable Manipulation

---
+++ lab01/src/22557138/lab-632D591B-1.py
@@ -0,0 +1,15 @@
+def check( userinput ) :
+    ret = False
+    # START: Write your code here
+    if int(userinput) >= 1 and int(userinput) <= 10:
+        ret = True
+    # END: Write you code here
+    return ret
+
+def main():
+    userinput = input("Enter a number between 1 and 10: ")
+    if check(userinput) :
+        print("The number is in between 1 to 10.")
+
+if __name__ == "__main__":
+    main()
---
+++ lab01/src/22557138/lab-632D7372-2.py
@@ -0,0 +1,11 @@
+myInput = ["This ", "is ", "a ", "test ", "message."]
+
+def mergeString(inputList): 
+    return "".join(myInput)
+ 
+def main(): 
+    aSentence = mergeString( myInput )
+    print(aSentence)
+
+if __name__ == "__main__":
+    main()
