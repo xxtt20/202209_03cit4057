05f59cdef426b1032127a37bc01a987929c28d03
kiki5548 <113424430+kiki5548@users.noreply.github.com>
2022-09-28 17:24:58 +0800

    Lab01 upload (#36)
    * Update students.csv
    
    * update
    
    * update
    
    * Classwork-1.3.
    
    * Correction
    
    Co-authored-by: edugarrick <106720230+edugarrick@users.noreply.github.com>

---
+++ lab01/src/22559647/lab-632D287D-2.py
@@ -0,0 +1,17 @@
+import string
+
+
+def getString() :
+    ret = ""
+    # START: Write your code here
+    str1 = "Holly"
+    str2 = "Dolly"
+    ret=str1+str2
+    # END: Write you code here
+    return ret
+
+def main():
+    print( getString())
+
+if __name__ == "__main__":
+    main()
---
+++ lab01/src/22559647/lab-632D64FF-1.py
@@ -0,0 +1,23 @@
+array_test = [1000, 22, 34, 58, 25, 1, 3, 19, 80]
+
+def sort(arr):
+    # START: Correct the mistake  
+    n = len(arr)
+    for i in range(n):
+     for j in range(0, n-i-1):
+        if arr[j] > arr[j+1]:
+          arr[j], arr[j+1] = arr[j+1], arr[j]
+    # END: Correct the mistake
+ 
+def main(): 
+    print("Original array is:")
+    for i in range(len(array_test)):
+        print("%d" % array_test[i], end=" ")
+    print("\n") 
+    sort(array_test)
+    print("Sorted array is:")
+    for i in range(len(array_test)):
+        print("%d" % array_test[i], end=" ")
+
+if __name__ == "__main__":
+    main()
