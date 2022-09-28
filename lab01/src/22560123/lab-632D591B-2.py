8940de9144cd6c71aaf96e3a0830beae46e80561
xxtt20 <113425177+xxtt20@users.noreply.github.com>
2022-09-28 17:25:54 +0800

    lab01 (#38)
    * Classwork-1.4.
    
    * lab01

---
+++ lab01/src/22560123/lab-632D591B-2.py
@@ -0,0 +1,21 @@
+def getDiscount( day, amount ) :
+    amountAfterDiscount = 0
+    # START: Write your code here
+    if day is "Monday":
+        amountAfterDiscount = (amount / 100)*0.9
+    else:
+        if day is "Tuesday":
+            amountAfterDiscount = (amount / 100)*0.95
+        else:
+            amountAfterDiscount = (amount /100)*0.99               
+    # END: Write you code here
+    return amountAfterDiscount
+
+def main():
+    day = input("Enter the current day: ")
+    amount = int(input("Enter the invoice amount: ")) 
+    amountAfterDiscount = getDiscount(day, amount)
+    print("Final amount is: {}".format(amountAfterDiscount) )
+
+if __name__ == "__main__":
+    main()
