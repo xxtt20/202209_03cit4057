cfd1d539d474f5ba67f6edd8ccda0ca614ff5e64
Garrick Ho <sales@cherryec.com>
2022-09-29 12:45:04 +0800

    Fixed after merged danielau0209 pull request.

--- lab01/src/lab-632D287D-2.py
+++ lab01/src/lab-632D287D-2.py
@@ -1,13 +1,9 @@
 def getString() :
     ret = ""
     # START: Write your code here
-    stir1="HOlly"
-    stir2="Dolly"
-    ret = stir1 + stir2
-    return ret 
-   
-    # END: Write you code here
 
+    # END: Write you code here
+    return ret
 
 def main():
     print( getString())
