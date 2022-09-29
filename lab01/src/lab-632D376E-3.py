c989b2696a72622355529fbb68649c37144364ea
Garrick Ho <sales@cherryec.com>
2022-09-29 12:59:24 +0800

    Resolved the issue after merged pull request from QiuXiaoJun22571287.

---
+++ lab01/src/lab-632D376E-3.py
@@ -0,0 +1,15 @@
+def getString(firstName, lastName) :
+    fullName = ""
+    # START: Write your code here
+    # Please use .format()
+
+    # END: Write you code here
+    return fullName
+
+def main():
+    firstName = input("Enter a first name: ") 
+    lastName = input("Enter a last name: ") 
+    print("Fullname: " + getString(firstName,lastName ))
+
+if __name__ == "__main__":
+    main()
---
+++ lab01/src/lab-632D591B-1.py
@@ -0,0 +1,14 @@
+def check( userinput ) :
+    ret = False
+    # START: Write your code here
+    
+    # END: Write you code here
+    return ret
+
+def main():
+    userinput = input("Enter a number between 1 and 10: ")
+    if check(userinput) :
+        print("The number is in between 1 to 10.")
+
+if __name__ == "__main__":
+    main()
