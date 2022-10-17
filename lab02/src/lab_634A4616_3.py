97f286b5056dca379a5393b891e5e00b1242b2d1
Garrick Ho <sales@cherryec.com>
2022-10-17 16:54:42 +0800

    New update for coming lesson.

---
+++ lab02/src/lab_634A4616_3.py
@@ -0,0 +1,15 @@
+def answer(weekd, value):
+    ret = 0
+    # END: You code here
+    return ret
+
+# Please don't change the code below!!!
+def main():
+    weekday = input("Enter the weekday: ") 
+    amt = input("Enter an amount: ") 
+    amt = float( "{:.2f}".format(float(amt)) )
+    print( answer(weekday,amt) )
+
+if __name__ == "__main__":
+    main()
+# Please don't change the code above!!!
