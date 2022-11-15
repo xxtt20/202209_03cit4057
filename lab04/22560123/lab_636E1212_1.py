9453f1055ff37bcb9a58846552e9c7db461400ff
xxtt20 <2957839161@qq.com>
2022-11-15 17:44:58 +0800

    Lab04

---
+++ lab04/22560123/lab_636E1212_1.py
@@ -0,0 +1,5 @@
+# Enter your code here
+import sys
+
+for path4Modules in sys.path:
+    print(path4Modules)
---
+++ lab04/22560123/lab_636E1212_2.py
@@ -0,0 +1,4 @@
+# Enter your code here
+from re import M
+from lab_package.namespace_a import my_function
+my_function()
