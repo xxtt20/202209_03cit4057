2c88eb940824791e90a2cc747cc390e715e8b94d
Garrick Ho <sales@cherryec.com>
2022-11-29 06:43:46 +0800

    New update for coming lesson.

---
+++ lab02/src/s2257172/lab_634AD398_1.py
@@ -0,0 +1,12 @@
+def answer(maxNumber):
+    # START: You code here
+    # END: You code here
+
+# Please don't change the code below!!!
+def main():
+    value = int(input("Please enter the maximum value: "))
+    answer(value)
+
+if __name__ == "__main__":
+    main()
+# Please don't change the code above!!!
---
+++ lab05/src/lab_6381D97A_3.py
@@ -0,0 +1,18 @@
+def get_set():
+    return my_set
+
+def answer(your_set):
+    return total
+
+def answer2(your_set):
+    return total
+
+def main():
+    numset = get_set()
+    print("Set:")
+    print(numset)
+    print("The sum of the values = {}".format(answer(numset)))
+    print("The sum of the values = {}".format(answer2(numset)))
+
+if __name__ == "__main__":
+    main()
