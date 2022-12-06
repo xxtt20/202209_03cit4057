ba736224ae358595ccd1906245a301f59eeb3762
liyuu-hkct <113425488+liyuu-hkct@users.noreply.github.com>
2022-12-06 12:01:36 +0800

    Lab 05 by Michael (#71)
    * Classwork-1.2.
    
    * Classwork-1.3.
    
    * Classwork-1.4.
    
    * Classwork-1.2.
    
    * Classwork-1.6
    
    * notes
    
    * Lab-632D591B: Comparisons and Booleans
    
    * Lab-632D376E: String Concatenations
    
    * Lab-632D7372 : String Join() and Split() Methods
    
    * Lab-632D64FF: Indentation
    
    * Lab-632D287D: Getting Started with Variable Manipulation
    
    * Setup my env.
    
    * Lab02
    
    * Lab02 revise01
    
    * Lab02 revise02
    
    * Lab03
    
    * Lab 03 revise01
    
    * Lab04 by Michael
    
    * Lab 05 by Michael

---
+++ lab05/src/22557138/lab_6381D97A_1.py
@@ -0,0 +1,17 @@
+def get_tuple():
+    my_tuple= (1,3,2,7,4,8)
+    return my_tuple
+
+def answer(your_tuple):
+    new_tuple = your_tuple + (9,)
+    return new_tuple
+
+def main():
+    numberlist = get_tuple()
+    print("Original tuple:")
+    print(numberlist)
+    print("New tuple:")
+    print(answer(numberlist))
+
+if __name__ == "__main__":
+    main()
