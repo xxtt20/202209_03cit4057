3e266d091ea392fc8dc613fa800cf47f385f4feb
bigwongq <113424511+bigwongq@users.noreply.github.com>
2022-12-24 09:16:27 +0800

    lab06 (#79)
    * Classwork-1.3.
    
    * lab01
    
    * lab01
    
    * lab01.
    
    * lab01.
    
    * 更新说明
    
    * new work
    
    * 'law02'
    
    * ''
    
    * 'lab03'
    
    * lab03
    
    * 'lab04'
    
    * lab04
    
    * 'lab05'
    
    * lab06
    
    * lab06
    
    Co-authored-by: edugarrick <106720230+edugarrick@users.noreply.github.com>

---
+++ lab06/src/22563705/lab_6393387A_1.py
@@ -0,0 +1,28 @@
+FILE = "22563705/message.txt"
+charCounts = [0]*26
+
+def findMaxCharInString( astring ):
+    maxCharIndex = 0
+    charCounts = [0]*26
+    for c in astring:
+        c = c.lower()
+        if 'a' <= c <= 'z':
+            charCounts[ord(c)-ord('a')] += 1
+    maxCharCount = max(charCounts)
+    maxCharIndex = charCounts.index(maxCharCount)
+    return maxCharIndex
+
+def getStringFromFile( filename ):
+    ret = ""
+    with open(filename, "r") as fp:
+        ret = fp.read()
+    return ret
+
+def main():
+    message = getStringFromFile(FILE)
+    print("Message is: {}".format(message))
+    print("The index with max freq: {}".format(
+        findMaxCharInString(message)))
+
+if __name__ == "__main__":
+    main()
